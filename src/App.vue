0419994a41ef48d132e86a5726144d18d8a97e43
东方 <1945458995@qq.com>
2022-04-11 17:09:54 +0800

    组件拆分

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        app
+        <Header></Header>
+        <router-view></router-view>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 export default {
     name: 'App',
+    components:{
+        Header,
+        Footer
+    }
 }
 </script>
 
